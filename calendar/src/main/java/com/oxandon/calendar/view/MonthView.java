641873feef7852762c0d0be3c8ed227008a3fbe7
FengPeng <726676435@qq.com>
2017-08-02 16:47:45 +0800

    添加日历标题显示年月

--- calendar/src/main/java/com/oxandon/calendar/view/MonthView.java
+++ calendar/src/main/java/com/oxandon/calendar/view/MonthView.java
@@ -2,16 +2,20 @@ package com.oxandon.calendar.view;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.oxandon.calendar.annotation.CalendarStatus;
 import com.oxandon.calendar.date.DateUtils;
 import com.oxandon.calendar.protocol.ICalendar;
 import com.oxandon.calendar.protocol.IMonthView;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -20,9 +24,13 @@ import java.util.Date;
 
 public class MonthView extends ViewGroup implements IMonthView {
     private final int HORIZONTAL_LINES = 6;
-    private final int LINE_HEIGHT = 1;
     private final DayView[] dayViews = new DayView[MAX_DAYS_OF_MONTH];
     private final View[] lines = new View[HORIZONTAL_LINES];
+    private TextView tvHead;
+    //dimens
+    private final int HEAD_HEIGHT;
+    private final int LINE_HEIGHT;
+
     private Date value;
     private int isTodayOfMonth = -1;
     //location
@@ -33,26 +41,25 @@ public class MonthView extends ViewGroup implements IMonthView {
     private int childHeight = 0;
 
     public MonthView(Context context) {
-        super(context);
-        initialize(context);
+        this(context, null);
     }
 
     public MonthView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initialize(context);
+        this(context, attrs, -1);
     }
 
     public MonthView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize(context);
-    }
-
-    /**
-     * 初始化
-     *
-     * @param context
-     */
-    private void initialize(Context context) {
+        HEAD_HEIGHT = ViewUtils.dp2px(context, 30);
+        LINE_HEIGHT = ViewUtils.dp2px(context, 0.5f);
+        //head text view
+        TextView head = new TextView(getContext());
+        head.setBackgroundColor(Color.parseColor("#90CFCFCF"));
+        head.setGravity(Gravity.CENTER);
+        head.setTextSize(15);
+        head.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+        addView(head);
+        tvHead = head;
         //DayView
         for (int i = 0; i < dayViews.length; i++) {
             dayViews[i] = new DayView(context);
@@ -72,8 +79,12 @@ public class MonthView extends ViewGroup implements IMonthView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int totalWidth = MeasureSpec.getSize(widthMeasureSpec);
         dayViews[0].measure(widthMeasureSpec, heightMeasureSpec);
-        int childrenHeight = dayViews[0].getMeasuredHeight() * MAX_ROWS;
-        setMeasuredDimension(totalWidth, childrenHeight + HORIZONTAL_LINES);
+        int childrenHeight = HEAD_HEIGHT;
+        childrenHeight += dayViews[0].getMeasuredHeight() * MAX_ROWS;
+        childrenHeight += HORIZONTAL_LINES * LINE_HEIGHT;
+        setMeasuredDimension(totalWidth, childrenHeight);
+        //measure head text view
+        tvHead.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(HEAD_HEIGHT, MeasureSpec.EXACTLY));
         //measure DayViews
         childWidth = totalWidth / WEEK_DAYS;
         childHeight = dayViews[0].getMeasuredHeight();
@@ -113,16 +124,19 @@ public class MonthView extends ViewGroup implements IMonthView {
 
     @Override
     protected void onLayout(boolean b, int left, int top, int right, int bottom) {
-        SplitLinesLayoutControl lineControl = new SplitLinesLayoutControl(lines);
         int offsetX = 0, offsetY = 0;
+        tvHead.layout(0, offsetY, getMeasuredWidth(), offsetY + tvHead.getMeasuredHeight());
+        offsetY += tvHead.getMeasuredHeight();
+
+        SplitLinesLayoutControl lineControl = new SplitLinesLayoutControl(lines);
         for (int i = 0; i < position; i++) {
             offsetX += childWidth;
         }
         offsetY = lineControl.layout(offsetY);
         int childBottom = offsetY + childHeight;
         boolean lastIsRightBound = false;//上一个是否是右边界
-        for (int index = 0, offset = position + 1; index < dayViews.length; index++, offset++) {
-            boolean rightBound = offset % 7 == 0;
+        for (int index = 0, move = position + 1; index < dayViews.length; index++, move++) {
+            boolean rightBound = move % 7 == 0;
             if (index < offset) {
                 int status = (lastIsRightBound || rightBound) ? CalendarStatus.STRESS : CalendarStatus.NORMAL;
                 dayViews[index].value(Integer.valueOf(index), status);
@@ -148,7 +162,7 @@ public class MonthView extends ViewGroup implements IMonthView {
 
     @Override
     public void desc(String desc, int status) {
-
+        tvHead.setText(desc);
     }
 
     @Override
@@ -156,6 +170,8 @@ public class MonthView extends ViewGroup implements IMonthView {
         position = calendar().firstDayOfMonthIndex(date);
         offset = calendar().maxDaysOfMonth(date);
         isTodayOfMonth = calendar().isTodayOfMonth(date);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        desc(sdf.format(date), CalendarStatus.NORMAL);
         if (null != value()) {
             requestLayout();
         }
---
+++ calendar/src/main/java/com/oxandon/calendar/view/ViewUtils.java
@@ -0,0 +1,32 @@
+package com.oxandon.calendar.view;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * Created by peng on 2017/8/2.
+ */
+
+public class ViewUtils {
+
+    public static int px2dp(Context context, float px) {
+        final float scale = context.getResources().getDisplayMetrics().densityDpi;
+        return (int) ((px * 160) / scale + 0.5f);
+    }
+
+    public static int dp2px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().densityDpi;
+        return (int) (dp * (scale / 160) + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spVal) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spVal, dm);
+    }
+
+    public static float px2sp(Context context, float pxVal) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return (pxVal / dm.scaledDensity);
+    }
+}
